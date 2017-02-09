9e39100efef4dfb9983d0f9762288a45a81dfca9
Kemal Akkoyun <kakkoyun@gmail.com>
2017-02-09 10:26:10 +0300

    Add command line arguments.

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ Command-line application that does a search and download.
 __author__ = 'kakkoyun@gmail.com (Kemal Akkoyun)'
 
 import os
+import sys
 import json
 import argparse
 
@@ -14,13 +15,30 @@ from google_image_search_service import GoogleImageSearchService
 
 
 def main():
-    API_KEY = os.environ['GOOGLE_API_KEY']
-    ENGINE_ID = os.environ['GOOGLE_SEARCH_ENGINE_ID']
+    parser = argparse.ArgumentParser(
+        description='Image Searcher: Provide parameters to search for images for given term.')
+    parser.add_argument('term', nargs='+',
+                        help='a search term to be searched')
+    parser.add_argument('--api_key',
+                        help='an API keys from Google Developer Console, (https://console.developers.google.com/apis/api/customsearch.googleapis.com/overview)')
+    parser.add_argument('--engine_id',
+                        help='an engine id from Google Custom Search Engine Console (https://cse.google.com/cse/all)')
 
-    service = GoogleImageSearchService(API_KEY, ENGINE_ID)
-    result = service.image_search('arnolfini paradoy')
+    args = parser.parse_args()
+    term = ' '.join(args.term)
+
+    try:
+        api_key = args.api_key if args.api_key else os.environ['GOOGLE_API_KEY']
+        engine_id = args.engine_id if args.engine_id else os.environ['GOOGLE_SEARCH_ENGINE_ID']
+    except KeyError, e:
+        print 'You need to provide %s, either by using optional arguments or environment variables.' % e
+        sys.exit()
+
+    service = GoogleImageSearchService(api_key, engine_id)
+    result = service.image_search(term)
 
     print json.dumps(result, indent=4, sort_keys=True)
+
 
 if __name__ == '__main__':
     main()
