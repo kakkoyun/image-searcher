2634d7ca3d60eab10d0b37be536d52ea4cedcb84
Kemal Akkoyun <kakkoyun@gmail.com>
2017-02-09 12:28:21 +0300

    Implement multi search with file. Fix minor conventions.

--- main.py
+++ main.py
@@ -10,18 +10,21 @@ import os
 import sys
 import json
 import argparse
-import logging, sys
+import logging
+import re
 
 from google_image_search_service import GoogleImageSearchService
 from image_download_service import ImageDownloadService
 from metadata_store_service import MetadataFileStorageService
 
 
-def initialize_argument_parser():
+def _initialize_argument_parser():
     parser = argparse.ArgumentParser(
         description='Image Searcher: Provide parameters to search for images for given term.')
-    parser.add_argument('term', nargs='+',
+    parser.add_argument('--term', nargs='+',
                         help='a search term to be searched')
+    parser.add_argument('--file', type=argparse.FileType('r'),
+                        help='a file that contains search terms')
     parser.add_argument('--count', type=int,
                         help='a parameter to specify how many images to download.')
     parser.add_argument('--api_key',
@@ -33,22 +36,51 @@ def initialize_argument_parser():
     return parser
 
 
-def initialize_logger(debug=False):
+def _initialize_logger(debug=False):
     if debug:
         logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
     else:
         logging.basicConfig(stream=sys.stderr, level=logging.INFO)
 
 
+def _sanitize_search_term(term):
+    return re.sub('\W+', '', term.replace(" ", "_").lower())
+
+
+def search_and_download_term(term, count, api_key, engine_id):
+    logger = logging.getLogger(__name__)
+    search_service = GoogleImageSearchService(api_key, engine_id)
+    result = search_service.call(term, count)
+    logger.debug(json.dumps(result, indent=4, sort_keys=True))
+    dir_name = os.path.join('data', _sanitize_search_term(term))
+    if not os.path.exists(dir_name):
+        os.makedirs(dir_name)
+    download_service = ImageDownloadService(dir_name)
+    storage_service = MetadataFileStorageService(dir_name)
+    for item in result:
+        url = item['link']
+        download_service.call(url)
+        storage_service.call(url, item)
+
+
 def main():
-    parser = initialize_argument_parser()
+    parser = _initialize_argument_parser()
 
     args = parser.parse_args()
-    term = ' '.join(args.term)
     count = args.count
     debug = args.debug
 
-    initialize_logger(debug)
+    terms = []
+    if args.term:
+        term = ' '.join(args.term)
+        terms = [term]
+
+    # File will have precedence
+    with args.file as file:
+        content = file.readlines()
+        terms = [x.strip() for x in content]
+
+    _initialize_logger(debug)
     logger = logging.getLogger(__name__)
 
     try:
@@ -58,21 +90,8 @@ def main():
         print 'You need to provide %s, either by using optional arguments or environment variables.' % e
         sys.exit()
 
-    search_service = GoogleImageSearchService(api_key, engine_id)
-    result = search_service.call(term, count)
-
-    logger.debug(json.dumps(result, indent=4, sort_keys=True))
-
-    dir_name = os.path.join('data', term.replace(" ", "_").lower())
-    if not os.path.exists(dir_name):
-        os.makedirs(dir_name)
-
-    download_service = ImageDownloadService(dir_name)
-    storage_service = MetadataFileStorageService(dir_name)
-    for item in result:
-        url = item['link']
-        download_service.call(url)
-        storage_service.call(url, item)
+    for term in terms:
+        search_and_download_term(term, count, api_key, engine_id)
 
 
 if __name__ == '__main__':
